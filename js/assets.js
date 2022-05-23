bfa06dcc1dc6bef7933373cd4f991da89f93775f
Syed Wajahat Ali <wajahat.klk25.com>
2022-05-23 20:55:57 +0200

    assets.js changes done

--- js/assets.js
+++ js/assets.js
@@ -1,27 +1,27 @@
 const ballImg = new Image();
-ballImg.src = '../images/images.jpeg';
+ballImg.src = './images/images.jpeg';
 
 const bGroundImg = new Image();
-bGroundImg.src = '../images/bg1.jpg';
+bGroundImg.src = './images/bg1.jpg';
 
 const score = new Image();
-score.src = '../images/life.png';
+score.src = './images/life.png';
 
 const pointImg = new Image();
-pointImg.src = '../images/score.png'
+pointImg.src = './images/score.png'
 
 // sounds
 const wallHit = new Audio();
-wallHit.src = '../Sounds/wall.mp3';
+wallHit.src = './Sounds/wall.mp3';
 
 const brickHit = new Audio ();
-brickHit.src = '../Sounds/brick_hit.mp3'; 
+brickHit.src = './Sounds/brick_hit.mp3'; 
 
 const paddleHit = new Audio();
-paddleHit.src = '../Sounds/paddle_hit.mp3';
+paddleHit.src = './Sounds/paddle_hit.mp3';
 
 const lifeLost = new Audio();
-lifeLost.src = '../Sounds/life_lost.mp3';
+lifeLost.src = './Sounds/life_lost.mp3';
 
 const gameOver = document.getElementById('gameover');
 const restart = document.getElementById('restart-btn');
