66f6adf6f1eb038edfec56cb52c06a581ac06bde
Syed Wajahat Ali <wajahat.klk25.com>
2022-10-07 21:02:38 +0200

    styles fixes

--- js/circle.js
+++ js/circle.js
@@ -1,12 +1,11 @@
 class Circle {
-    constructor (x, y, size, dx, dy, speed){
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.dx = dx;
-        this.dy = dy; 
-        
-    }
+  constructor(x, y, size, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.dx = dx;
+    this.dy = dy;
+  }
 }
 
 // this is Ball in the GAME
--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
-
-
-class Game{
+class Game {
   constructor(context, canvas) {
     this.ctx = context;
     this.canvas = canvas;
-    this.spaceBar = new Player (450, 555, 200, 40);
-    this.circle = new Circle (200, 200, 20, 10, 10);
+    this.spaceBar = new Player(450, 555, 200, 40);
+    this.circle = new Circle(200, 200, 20, 5, 5);
     this.bricks = [];
-    this.bricks2 =  [];
+    this.bricks2 = [];
     this.gameOver = false;
     this.points = 0;
     this.ctx.lineWidth = 5;
@@ -15,38 +13,56 @@ class Game{
 
   // draw spaceBar in canvas
   _drawSpaceBar() {
-    this.ctx.fillStyle = 'white';
-    this.ctx.fillRect(this.spaceBar.x, this.spaceBar.y, this.spaceBar.width, this.spaceBar.height);
-    this.ctx.strokeStyle = 'purple';
-    this.ctx.strokeRect(this.spaceBar.x, this.spaceBar.y, this.spaceBar.width, this.spaceBar.height);
-    
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(
+      this.spaceBar.x,
+      this.spaceBar.y,
+      this.spaceBar.width,
+      this.spaceBar.height
+    );
+    this.ctx.strokeStyle = "purple";
+    this.ctx.strokeRect(
+      this.spaceBar.x,
+      this.spaceBar.y,
+      this.spaceBar.width,
+      this.spaceBar.height
+    );
   }
-// draw life img/icon
-  _drawLife () {
+  // draw life img/icon
+  _drawLife() {
     this.ctx.drawImage(score, 550, 950);
   }
-// draw ball
-  _drawCircle () {
+  // draw ball
+  _drawCircle() {
     this.ctx.beginPath();
-    this.ctx.arc(this.circle.x, this.circle.y, this.circle.size, 0, Math.PI * 2);
-    this.ctx.fillStyle = 'grey';
+    this.ctx.arc(
+      this.circle.x,
+      this.circle.y,
+      this.circle.size,
+      0,
+      Math.PI * 2
+    );
+    this.ctx.fillStyle = "grey";
     this.ctx.fill();
-    this.ctx.strokeStyle = 'yellow'; 
+    this.ctx.strokeStyle = "yellow";
     this.ctx.stroke();
     this.ctx.closePath();
   }
-// collisions with the walls, roof & floor of the canvas
- _bounceWalls(){
-     this.circle.x += this.circle.dx;
-     this.circle.y += this.circle.dy;
-     if (this.circle.x + this.circle.size > 1000|| this.circle.x - this.circle.size < 0){
-       this.circle.dx *= -1;
-       wallHit.play();
-      }
-      
-    if(this.circle.y - this.circle.size <= 0){
-      this.circle.dy *=-1;
-    } else if(this.circle.y + this.circle.size > 600) {
+  // collisions with the walls, roof & floor of the canvas
+  _bounceWalls() {
+    this.circle.x += this.circle.dx;
+    this.circle.y += this.circle.dy;
+    if (
+      this.circle.x + this.circle.size > 1000 ||
+      this.circle.x - this.circle.size < 0
+    ) {
+      this.circle.dx *= -1;
+      wallHit.play();
+    }
+
+    if (this.circle.y - this.circle.size <= 0) {
+      this.circle.dy *= -1;
+    } else if (this.circle.y + this.circle.size > 600) {
       if (this.spaceBar.life < 1) {
         this._gameOver();
       } else {
@@ -54,102 +70,116 @@ class Game{
         lifeLost.play();
       }
     }
- }
-
-
-// ball collision with the space bar
-
- _spaceBarCollision () {
-  if (this.circle.y + this.circle.size > 600 || (this.circle.y + this.circle.size > this.spaceBar.y && (this.circle.x > this.spaceBar.x && this.circle.x <this.spaceBar.x+this.spaceBar.width) ) || this.circle.y - this.circle.size < 0){
-    this.circle.dy *= -1;
-    paddleHit.play();
   }
- }
- // pre-loaded bricks in game
 
- _drawInitialBricks(){
+  // ball collision with the space bar
+
+  _spaceBarCollision() {
+    if (
+      this.circle.y + this.circle.size > 600 ||
+      (this.circle.y + this.circle.size > this.spaceBar.y &&
+        this.circle.x > this.spaceBar.x &&
+        this.circle.x < this.spaceBar.x + this.spaceBar.width) ||
+      this.circle.y - this.circle.size < 0
+    ) {
+      this.circle.dy *= -1;
+      paddleHit.play();
+    }
+  }
+  // pre-loaded bricks in game
+
+  _drawInitialBricks() {
     let startX = 50;
     let startX2 = 50;
     let startY = 90;
-    let startY2 = 150; 
-    if (this.bricks.length === 0){
-
-      for (let c = 0; c < 4; c++){
+    let startY2 = 150;
+    if (this.bricks.length === 0) {
+      for (let c = 0; c < 4; c++) {
         let newBrick = new Brick(startX, startY, 150, 50, this.status);
         this.bricks.push(newBrick);
         startX = startX + 250;
       }
-   }
-   // second row of bricks
-    if (this.bricks2.length === 0){
-
-      for (let c = 0; c< 4; c++){
+    }
+    // second row of bricks
+    if (this.bricks2.length === 0) {
+      for (let c = 0; c < 4; c++) {
         let newBrick = new Brick(startX2, startY2, 150, 50, this.status);
         this.bricks2.push(newBrick);
         startX2 = startX2 + 250;
       }
     }
- }
- // drawing bricks in game
-  _drawBricks () {
-    this.ctx.fillStyle = 'brown';
-    this.bricks.forEach(brick => this.ctx.fillRect(brick.x, brick.y, brick.width, brick.height))
-    this.bricks2.forEach(brick => this.ctx.fillRect(brick.x, brick.y, brick.width, brick.height)
-    )
+  }
+  // drawing bricks in game
+  _drawBricks() {
+    this.ctx.fillStyle = "brown";
+    this.bricks.forEach((brick) =>
+      this.ctx.fillRect(brick.x, brick.y, brick.width, brick.height)
+    );
+    this.bricks2.forEach((brick) =>
+      this.ctx.fillRect(brick.x, brick.y, brick.width, brick.height)
+    );
   }
 
   //collisions check with bricks
-  _checkCollision()  {
-    this.bricks.forEach(elem =>{
-      if((this.circle.x > elem.x && this.circle.x <= elem.x + elem.width) && (this.circle.y > elem.y && this.circle.y < elem.y + elem.height)){
+  _checkCollision() {
+    this.bricks.forEach((elem) => {
+      if (
+        this.circle.x > elem.x &&
+        this.circle.x <= elem.x + elem.width &&
+        this.circle.y > elem.y &&
+        this.circle.y < elem.y + elem.height
+      ) {
         brickHit.play();
         let index = this.bricks.indexOf(elem);
         this.bricks.splice(index, 1);
-        if(this.bricks.length === 0 && this.bricks2.length === 0) {
-            this._win();
-          }else {
-          this.points += 1;
-          }
-      }
-  
-    } )
-    
-    this.bricks2.forEach(elem =>{
-      if((this.circle.x > elem.x && this.circle.x <= elem.x + elem.width) && (this.circle.y > elem.y && this.circle.y < elem.y + elem.height)){
-        brickHit.play();
-        let index = this.bricks2.indexOf(elem);
-        this.bricks2.splice(index, 1);
-        if(this.bricks.length === 0 && this.bricks2.length === 0){
+        if (this.bricks.length === 0 && this.bricks2.length === 0) {
           this._win();
         } else {
           this.points += 1;
         }
       }
-  } )
-    
+    });
+
+    this.bricks2.forEach((elem) => {
+      if (
+        this.circle.x > elem.x &&
+        this.circle.x <= elem.x + elem.width &&
+        this.circle.y > elem.y &&
+        this.circle.y < elem.y + elem.height
+      ) {
+        brickHit.play();
+        let index = this.bricks2.indexOf(elem);
+        this.bricks2.splice(index, 1);
+        if (this.bricks.length === 0 && this.bricks2.length === 0) {
+          this._win();
+        } else {
+          this.points += 1;
+        }
+      }
+    });
   }
-    
-  _gameOver(){
+
+  _gameOver() {
     this.gameOver = true;
-    const losePage = document.getElementById('lose-page');
-    losePage.style.display= 'block';
+    const losePage = document.getElementById("lose-page");
+    losePage.style.display = "block";
     this.canvas.remove();
   }
-  _win(){
-    const winPage = document.getElementById('win-page');
-    winPage.style.display= 'block';
+  _win() {
+    const winPage = document.getElementById("win-page");
+    winPage.style.display = "block";
     this.canvas.remove();
-    document.getElementsByTagName('canvas')[0].remove();
+    document.getElementsByTagName("canvas")[0].remove();
   }
 
   _assignControls() {
     // Controles del teclado
-    document.addEventListener('keydown', (event) => {
+    document.addEventListener("keydown", (event) => {
       switch (event.code) {
-        case 'ArrowLeft':
+        case "ArrowLeft":
           this.spaceBar.moveLeft();
           break;
-        case 'ArrowRight':
+        case "ArrowRight":
           this.spaceBar.moveRight();
           break;
         default:
@@ -158,37 +188,37 @@ class Game{
     });
   }
 
-_drawLife() { 
-  
-  this.ctx.font = '50px Germania One';
- 
-  this.ctx.drawImage(score, 850, 5, 100, 100);
-  this.ctx.drawImage(pointImg, 40, 30, 50, 50);
-  this.ctx.fillText(this.spaceBar.life, 940, 70); 
-  this.ctx.fillText(this.points,100, 70);
-}
+  _drawLife() {
+    this.ctx.font = "50px Germania One";
 
-  _clean(){
+    this.ctx.drawImage(score, 850, 5, 100, 100);
+    this.ctx.drawImage(pointImg, 40, 30, 50, 50);
+    this.ctx.fillText(this.spaceBar.life, 940, 70);
+    this.ctx.fillText(this.points, 100, 70);
+  }
+
+  _clean() {
     this.ctx.clearRect(0, 0, 1000, 600);
   }
 
-  _gOver () {
-    gameOver.style.display = 'block';
-  
+  _gOver() {
+    gameOver.style.display = "block";
   }
-  
+
   _update() {
     this._clean();
     this._drawLife();
-    this._spaceBarCollision ();
+    this._spaceBarCollision();
     this._checkCollision();
     this._bounceWalls();
     this._drawBricks();
     this._drawCircle();
     this._drawSpaceBar();
-    
-    if(!this.gameOver){    
-    window.requestAnimationFrame(() =>{this._update()});
+
+    if (!this.gameOver) {
+      window.requestAnimationFrame(() => {
+        this._update();
+      });
     }
   }
 
